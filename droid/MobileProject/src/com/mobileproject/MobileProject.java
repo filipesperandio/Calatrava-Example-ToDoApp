7296eb7bd00bcb759f873989844d88eed38e9523
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2013-02-17 18:37:50 -0300

    base_line

---
+++ droid/MobileProject/src/com/mobileproject/MobileProject.java
@@ -0,0 +1,23 @@
+package com.mobileproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.calatrava.bridge.Launcher;
+
+public class MobileProject extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Launcher.launchKernel("com.mobileproject", this, getApplication(), new Runnable() {
+          public void run() {
+            Launcher.launchFlow("example.converter.start");
+          }
+        });
+    }
+}
